21f778e89822aaa66af1d4df08a3751061f7e1b1
mudler <mudler@dark-lab.net>
2015-03-27 18:20:31 +0100

    using custom nvidia-userspace for recompilation script

--- bin/recompile_drivers.sh
+++ bin/recompile_drivers.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 . /spike/bin/kernels.inc
-packages="x11-drivers/ati-drivers::sabayon-distro x11-drivers/nvidia-drivers::spike sys-power/bbswitch x11-drivers/nvidia-userspace::sabayon-distro app-laptop/nvidiabl::sabayon app-emulation/virtualbox-guest-additions app-emulation/virtualbox-modules x11-drivers/xf86-video-virtualbox"
+packages="x11-drivers/ati-drivers::sabayon-distro x11-drivers/nvidia-drivers::spike sys-power/bbswitch x11-drivers/nvidia-userspace::spike app-laptop/nvidiabl::sabayon app-emulation/virtualbox-guest-additions app-emulation/virtualbox-modules 
+x11-drivers/xf86-video-virtualbox"
 
 rm -rf /usr/portage/packages/*
 emerge -B virtualbox-bin
